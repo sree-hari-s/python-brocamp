71bc8b02830912e15141afd71fb4f9ca5e9d7685
Sreehari S <sreeharis1999@gmail.com>
2022-10-30 22:59:02 +0530

    first commit

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -3,9 +3,13 @@ import random
 def get_choices():
     player_choice = input("Enter a choice(rock, paper, scissors):")
     options = ["rock", "paper", "scissors"]
-    computer_choice = random.choice(options)
-    choices = {"player": player_choice, "computer": computer_choice}
-    return choices
+    if player_choice in options:
+        computer_choice = random.choice(options)
+        choices = {"player": player_choice, "computer": computer_choice}
+        return choices
+    else:
+        print("Invalid Input")
+        exit()
 
 
 def check_win(player, computer):
