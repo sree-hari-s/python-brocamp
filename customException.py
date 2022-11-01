e881aaf5736eaa53e1cf681ce9bb14aa9b5ddfa2
Sreehari S <sreeharis1999@gmail.com>
2022-11-01 17:01:46 +0530

    changes

--- customException.py
+++ customException.py
@@ -1,3 +1,4 @@
+#custom exception
 try:
     raise Exception("you are ded")
 except Exception as error:
@@ -6,3 +7,16 @@ else:
     print("you are alive")
 finally:
     print("reborn again")
+    
+
+print("---------------------------------------")
+#custom exception using class
+class dogNotFound(Exception):
+    print("inside class")
+
+try:
+    raise dogNotFound()
+except dogNotFound:
+    print("Dog not found ")
+finally:
+    print("Dog found finally")
